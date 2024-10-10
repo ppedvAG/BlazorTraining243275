5c660b621fd3acf1c69b585cd294183d6a530882
Hannes Preishuber <hannesp@ppedv.de>
2024-10-10 16:48:00 +0200

    TODO nach MVVM und REST Service per httpclient

---
+++ BlazorTraining243275/Components/Pages/Modul4/ToDoModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorTraining243275.Components.Pages.Modul4
+{
+    public class ToDoModel
+    {
+
+        public Todo[] todos { get; set; }
+        public int total { get; set; }
+        public int skip { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class Todo
+    {
+        public int id { get; set; }
+        public string todo { get; set; }
+        public bool completed { get; set; }
+        public int userId { get; set; }
+    }
+
+}
---
+++ BlazorTraining243275/Components/Pages/Modul4/TodoVM.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorTraining243275.Components.Pages.Modul4
+{
+    public class TodoVM
+    {
+        public ToDoModel ToDoObjekt { get; set; }
+        IHttpClientFactory http;
+        public TodoVM(IHttpClientFactory _http)
+        {
+             http = _http;
+            using (var client = http.CreateClient())
+            {
+               //ToDoObjekt = await client.GetFromJsonAsync<ToDoModel>("https://dummyjson.com/todos");
+            }
+
+        }
+    }
+}
