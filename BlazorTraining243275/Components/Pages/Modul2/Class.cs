16a285d0ceb641ababbee9bc53af90795372686d
Hannes Preishuber <hannesp@ppedv.de>
2024-10-10 11:36:17 +0200

    Demo Attribute auf Klasse

---
+++ BlazorTraining243275/Components/Pages/Modul2/Class.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BlazorTraining243275.Components.Pages.Modul2
+{
+    public class Class
+    {
+        [Authorize(Roles ="Admin")]
+        void methode()
+        {
+
+        }
+    }
+}
