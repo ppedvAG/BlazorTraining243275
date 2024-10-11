36ea0c4c6acdf1b6095330651ed4feb598959917
Hannes Preishuber <hannesp@ppedv.de>
2024-10-11 11:37:11 +0200

    Validation mit Dataanotation und Editform

--- BlazorTraining243275/Components/Pages/modul5/Person.cs
+++ BlazorTraining243275/Components/Pages/modul5/Person.cs
@@ -8,10 +8,12 @@ namespace BlazorTraining243275.Components.Pages.modul5
         [Required(ErrorMessage ="Da muss was rein")]
         [StringLength(10,ErrorMessage ="zwishcne 3 und 10",MinimumLength =3)]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Da muss was rein")]
+        [Range(150, 190, ErrorMessage = "zwischen 150 und 190")]
         public int Gross { get; set; }
+
         [Required(ErrorMessage = "Da muss was rein")]
-        [Range(150,190,ErrorMessage ="zwischen 150 und 190")]
         public DateTime GebDate { get; set; }
     }
 }
