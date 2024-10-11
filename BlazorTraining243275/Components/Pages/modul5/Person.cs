af8a3e5078f9512271668de203ac9a99d1be39c5
Hannes Preishuber <hannesp@ppedv.de>
2024-10-11 11:17:16 +0200

    Person Klasse mit DataAnnotions für Validierung

---
+++ BlazorTraining243275/Components/Pages/modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTraining243275.Components.Pages.modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(10,ErrorMessage ="zwishcne 3 und 10",MinimumLength =3)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(150,190,ErrorMessage ="zwischen 150 und 190")]
+        public DateTime GebDate { get; set; }
+    }
+}
