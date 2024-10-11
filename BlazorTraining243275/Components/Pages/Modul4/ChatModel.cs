73af794a97a5a20b1430604be279224981612b24
Hannes Preishuber <hannesp@ppedv.de>
2024-10-11 09:01:50 +0200

    Chat Model Klasse

---
+++ BlazorTraining243275/Components/Pages/Modul4/ChatModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorTraining243275.Components.Pages.Modul4
+{
+    public class ChatModel
+    {
+        public string Nachricht { get; set; }
+        public string Sender { get; set; }
+    }
+}
